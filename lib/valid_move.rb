a060922dc0c2381dfb576d76a301ce66c8f663e7
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-01-31 18:40:27 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
+def input_to_index(input)
+  input = input.to_i
+  input - 1
+  input = index
+end
+
 
 def valid_move?(board, index)
   if board[index] >= 0 && board[index] <= 8 && position_taken?
