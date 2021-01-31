7400e433f13c8557b7ac62ef029b3b9369cf413a
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-01-31 18:59:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 
 def valid_move?(board, index)
-   if board[index] >= board[0] && board[index] <= board[8]
+   if board[index] >= 0 && board[index] <= 8
      true
    else
      false
